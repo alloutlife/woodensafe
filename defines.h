7e67ec19333f6be224d65a9f42a4a87884f87f8b
alloutlife <avkushnarev@yandex.ru>
2020-01-08 22:35:03 +0300

    Adding motor powering control routine
    MOTOL_POWERING_CTRL solves the problems:
      1. Current consumption by Servo: ~5ma @ 7.4V
      2. Device initialization: being powered the Servo turns the shaft randomly. This leads to the brief opening of a closed safe during the power on procedure (~1 sec)

--- defines.h
+++ defines.h
@@ -1,5 +1,5 @@
 #define LED_GREEN_PORT  5   /* Be careful to choose from those pins where analogWrite is suitable */
-#define LED_WHITE_PORT  3   /* Also note pins 9 and 10 are not suitable for that because of Servo.detachx */
+#define LED_WHITE_PORT  3   /* Also note pins 9 and 10 are not suitable for that because of Servo.detach */
 
 #define MOTOR_CTRL_PORT 9
 #define SOLENOID_CTRL_PORT 9
@@ -7,3 +7,5 @@
 
 #define SAFE_OPEN_PORT  12
 #define PROG_BUTTON_PORT  7
+
+#define MOTOL_POWERING_CTRL 4
--- motor.ino
+++ motor.ino
@@ -32,20 +32,30 @@ Servo _g_servo;    // create servo object to control a servo
 
 void _setup_safe_door( void )
 {
+  // MOTOL_POWERING_CTRL solves the problems:
+  //      1. Current consumption by Servo: ~5ma @ 7.4V
+  //      2. Device initialization: being powered the Servo turns the shaft randomly
+  //         this leads to the brief opening of a closed safe during the power on procedure (~1 sec)
+  pinMode( MOTOL_POWERING_CTRL, OUTPUT );
+  digitalWrite( MOTOL_POWERING_CTRL, 0 );
 }
 
 void _unlock_safe_door( void )
 {
+  digitalWrite( MOTOL_POWERING_CTRL, 1 ); // Power on the motor
   _g_servo.attach( MOTOR_CTRL_PORT );    //attachs the servo
   _g_servo.write( 30 );   // to 0 degrees
   delay( 1000 );
   _g_servo.detach();
+  digitalWrite( MOTOL_POWERING_CTRL, 0 ); // Power off the motor
 }
 
 void _lock_safe_door( void )
 {
+  digitalWrite( MOTOL_POWERING_CTRL, 1 ); // Power on the motor
   _g_servo.attach( MOTOR_CTRL_PORT );    //attachs the servo
   _g_servo.write( 0 );   // back to 30 degrees
   delay( 1000 );
   _g_servo.detach();
+  digitalWrite( MOTOL_POWERING_CTRL, 0 ); // Power off the motor
 }

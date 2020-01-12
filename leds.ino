9a65385d66866ea821db6a7da915a9067af740a9
alloutlife <avkushnarev@yandex.ru>
2020-01-12 12:00:29 +0300

    Tuning the delays between kepresses

--- leds.ino
+++ leds.ino
@@ -43,17 +43,20 @@ void _white_led_blink_1( void ) {
 void _led_with_buzzer_keypress_echo( int nLedNo )
 {
   const int _port = nLedNo;
-  const int delayVal = 300;
+  int delayVal = 300;
+  if( nLedNo == LED_WHITE_PORT ) {
+    delayVal = 150;
+  }
   
   for (int a=0; a<=255;a++)  // Loop, PWM control of LED brightness increase
   {
-    analogWrite(_port,a);   // PWM output value a (0~255)
-    delayMicroseconds(delayVal);                // The duration of the current brightness level. 15ms           
+    analogWrite(_port,a);   // PWM output value a (0 .. 200)
+    delayMicroseconds(delayVal);                // The duration of the current brightness level
   }
 
   _start_play_button_pressed_sound();
   
-  delay(50);                // 100ms delay
+  delay(20);                // 100ms delay
 
   _stop_play_button_pressed_sound();
 
@@ -61,8 +64,8 @@ void _led_with_buzzer_keypress_echo( int nLedNo )
   
   for (int a=255; a>=0;a--)  // Loop, PWM control of LED brightness Reduced
   {
-    analogWrite(_port,a);   // PWM output value a (255~0)
-    delayMicroseconds(delayVal);                // The duration of the current brightness level. 15ms 
+    analogWrite(_port,a);   // PWM output value a (200 .. 0)
+    delayMicroseconds(delayVal);                // The duration of the current brightness level
   }
 }
 
